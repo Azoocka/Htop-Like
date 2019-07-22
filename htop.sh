35ae213b99b7cfc49679e2a89a2fe039fbdaa8d1
Azoockaz <Melvinn.Melou@gmail.com>
2019-07-22 17:58:02 +0200

    add htop

---
+++ htop.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+free -m |awk 'NR==2{printf " Memory RAM Utilisée: %s/%sMB (%.2f%%)\n" , $3,$2,$3*100/$2 }'
+df -h | awk '$NF=="/" {printf "Utilisation du disque: %d/ %dGB (%s) \n" , $3,$2,$5}'
+top -bn1 | grep load  | awk '{printf " Charge CPU: %.2f\n" , $(NF-2)}'
