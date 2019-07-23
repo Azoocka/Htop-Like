2a236025ca7c450d0622878274ea4fc712c3fc2a
Azoockaz <Melvinn.Melou@gmail.com>
2019-07-23 14:58:12 +0200

    git commit

--- htopv3.sh
+++ htopv3.sh
@@ -15,29 +15,28 @@ echo
 
 case $choice in
     1)
-    MemTotal=$(grep MemTotal /proc/meminfo | awk '{print $2}')
-    MemFree=$(grep MemFree /proc/meminfo | awk '{print $2}')
-     echo $(($MemTotal-$MemFree))
+        MemTotal=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+        MemFree=$(grep MemFree /proc/meminfo | awk '{print $2}')
+        MemUse=$((($MemTotal-$MemFree)/1024))
+        echo "Votre Consommation est de $MemUse Mb"
     ;;
     2)
-    MemTotal=$(grep MemTotal /proc/meminfo | awk '{print $2}')
-    MemFree=$(grep MemFree /proc/meminfo | awk '{print $2}')
-     echo $(($MemTotal-$MemFree))
-
+        MemTotal=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+        MemFree=$(grep MemFree /proc/meminfo | awk '{print $2}')
+        MemUse=$(((($MemTotal-$MemFree)/1024)/1024))
+        echo "Votre Consommation est de $MemUse Gb"
     ;;
     3)
-
     ;;
-    4
-    awk '{printf("%d:%02d:%02d:%02d\n",($1/60/60/24),($1/60/60%24),($1/60%60),($1%60))}' /proc/uptime
+    4)
+        awk '{printf("%d:%02d:%02d:%02d\n",($1/60/60/24),($1/60/60%24),($1/60%60),($1%60))}' /proc/uptime
     ;;
     0)
     echo " Très bien, à très bientöt !"
     break
     ;;
     *)
-    echo " Ce choix n'est pas valide , merci de bien vouloir indiquer une valeur entre 0 et 3"
+    echo " Ce choix n'est pas valide , merci de bien vouloir indiquer une valeur entre 0 et 3 : "
     ;;
 esac
 done
-
